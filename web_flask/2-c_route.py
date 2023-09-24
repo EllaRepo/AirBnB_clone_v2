5f463b532118b1e2ac6d629e93466720cb176147
EllaRepo <eliaberango@gmail.com>
2023-09-24 13:48:11 -0400

    Add 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Function called with '/' route
+    Returns 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function called with '/hbnb' route
+    Returns 'HBNB'
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Function called with '/c/<text>' route
+    Returns 'C' follwed by the value of text
+    """
+    return 'C %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
